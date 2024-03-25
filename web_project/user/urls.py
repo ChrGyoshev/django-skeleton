37cb90ce065e37fba1131995a944ba80b47ef4c2
ChrGyoshev <krischan@abv.bg>
2024-03-25 16:00:01 +0200

    added JWT token

--- web_project/user/urls.py
+++ web_project/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from web_project.user.views import index
+from web_project.user.views import LoginView, RestrictedView
 
 
 urlpatterns = [
-    path('', index, name='home'),
+    
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/restricted/', RestrictedView.as_view(), name='restricted'),
+    
 ]
--- web_project/user/views.py
+++ web_project/user/views.py
@@ -1,7 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(email=email, password=password)
+        refresh = RefreshToken.for_user(user)
+        return JsonResponse({
+            'refreshToken': str(refresh),
+            'accessToken':str(refresh.access_token),
+        })
+
+
+
+class RestrictedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        return JsonResponse({'response': "YOU ARE ALLOWED HERE"})
+
+    
+        
+    
+   
