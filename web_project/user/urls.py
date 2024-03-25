5d3ef4ce81829e1d500052f5cbd2a20db7f8e804
ChrGyoshev <krischan@abv.bg>
2024-03-25 21:26:04 +0200

    added url paths

--- web_project/user/urls.py
+++ web_project/user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from web_project.user.views import LoginView, RestrictedView
+from web_project.user.views import  LoginView, RestrictedView
 
 
 urlpatterns = [
     
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/restricted/', RestrictedView.as_view(), name='restricted'),
-    
+   
 ]
--- web_project/user/views.py
+++ web_project/user/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken 
+from django.contrib.auth import authenticate,login
 from rest_framework.permissions import IsAuthenticated
+from django.views import generic as View
 
 
 class LoginView(APIView):
@@ -12,21 +13,27 @@ class LoginView(APIView):
         password = request.data.get('password')
 
         user = authenticate(email=email, password=password)
-        refresh = RefreshToken.for_user(user)
-        return JsonResponse({
-            'refreshToken': str(refresh),
+        if user is not None:
+            login(request,user)
+            refresh = RefreshToken.for_user(user)
+            return JsonResponse({
+                'refreshToken': str(refresh),
             'accessToken':str(refresh.access_token),
+            "user": request.user.email
         })
-
-
+    
 
 class RestrictedView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self,request):
-        return JsonResponse({'response': "YOU ARE ALLOWED HERE"})
+        user = request.user
+
+        return JsonResponse({'response': "YOU ARE ALLOWED HERE",
+                             "user": user.email})
 
     
-        
+
+
     
    
